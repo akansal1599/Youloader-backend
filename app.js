badb8ac85a8a52a942fee3f350f0939bb2190e69
akansal159 <akansal1599@Gmail.com>
2020-05-28 04:53:45 +0530

    first version

---
+++ app.js
@@ -0,0 +1,66 @@
+// Import dependencies
+const express = require("express");
+const youtubedl = require("youtube-dl");
+
+const app = express();
+const port = 5000;
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
+app.get("/", async (req, res) => {
+
+    if (!req.query.url || req.query.url.length===0) {
+        // Send error response
+        res.status(400).send({
+            ok: false,
+            error: "No URL"
+        });
+        return;
+    }
+
+    // How to Write a JavaScript Promise - https://www.freecodecamp.org/news/how-to-write-a-javascript-promise-4ed8d44292b8
+    const download = new Promise(function (resolve, reject) {
+
+        // Download the YouTube video
+        youtubedl.exec(req.query.url, ["--format", "bestvideo[height<=1080]+bestaudio/best[height<=1080]"], { cwd: __dirname },
+            (err, output) => {
+                if (err) reject(err);
+
+                resolve(output);
+            })
+    });
+
+    try {
+        console.log(__dirname);
+        // Await the downloader
+        const output = await download;
+
+        // Send response
+        res.status(200).send({
+            ok: true,
+            result: output
+        });
+
+
+    } catch (error) {
+
+        // Send error response
+        res.status(500).send({
+            ok: false,
+            error: err.message
+        });
+
+    }
+
+});
+// () => console.log(`Downloader listening at http://localhost:${port}`)
+app.listen(process.env.PORT || port);
