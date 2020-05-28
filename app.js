1e64f58fbf5755f00b26a953aa7b70ac648f7ceb
akansal159 <akansal1599@Gmail.com>
2020-05-28 07:56:33 +0530

    env.port corrected 9

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get("/", async (req, res) => {
         // Download the YouTube video
         // console.log(path)
         // var fileLocation = path.join('./uploads',file);
-        youtubedl.exec(req.query.url, ["--format=bestvideo[height<=1080]+bestaudio/best[height<=1080]",`--output=~/${pathObj}/Youloader/%(title)s.%(ext)s)`], { cwd: __dirname },
+        youtubedl.exec(req.query.url, ["--format=bestvideo[height<=1080]+bestaudio/best[height<=1080]",`--output=~/${pathObj}/%(title)s.%(ext)s)`], { cwd: __dirname },
             (err, output) => {
                 if (err) reject(err);
                 file=output;
